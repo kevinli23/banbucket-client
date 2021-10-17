74341f75ffa27f47f6f21ed479f1952e03c83464
Kevin <cskevinli23@gmail.com>
2021-10-17 16:35:05 -0400

    add amount and change icon

--- src/components/ClaimBox.jsx
+++ src/components/ClaimBox.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import { Input, Button, Text, Heading} from '@chakra-ui/react';
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 import IconRow from './IconRow';
 import QRCode from 'react-qr-code';
+import { ReactComponent as BanIcon } from '../banicon.svg';
 
 const ClaimBox = () => {
 	const [addr, setAddr] = useState('');
@@ -13,6 +14,26 @@ const ClaimBox = () => {
 	const [msg, setMsg] = useState('Please enter a valid Banano address');
 	const [showMsg, setShowMsg] = useState(false);
 	const [loading, setLoading] = useState(false);
+	const [amount, setAmount] = useState(0)
+
+	useEffect(() => {
+		(
+			async () => {
+				await fetch("https://banbucket.herokuapp.com/api/v1/amount", {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}).then(async (res) => {
+					const json = await res.json()
+					setAmount(json.amount)
+				}).catch((_err) => {
+					setAmount(0)
+					return
+				})
+			}
+		)()
+	})
 
 	const captchaRef = useRef(null);
 
@@ -25,7 +46,11 @@ const ClaimBox = () => {
 
 	return (
 		<>
-		<Heading mt="5px" color="#E4C703" fontFamily="Aleo, serif" size="3xl">BanBucket</Heading>
+		<Heading mt="20px" color="#E4C703" fontFamily="Aleo, serif" size="3xl">BanBucket</Heading>
+		<div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
+			<Text fontSize="xl" m="5px">{amount}</Text>
+			<BanIcon style={{ width: "20px", height: "20px" }} />
+		</div>
 			<div style={{ minHeight: '30px' }}>
 				{((!isValid && addr !== '') || showMsg) && (
 					<Text fontSize="lg" color={isErr ? 'red.500' : 'green.500'}>
@@ -77,37 +102,54 @@ const ClaimBox = () => {
 					onClick={async () => {
 						setLoading(true);
 						captchaRef.current.resetCaptcha()
-						const requestOptions = {
+						var failed = false
+
+						await fetch('https://api-beta.banano.cc', {
 							method: 'POST',
 							headers: {
 								'Content-Type': 'application/json',
 							},
-							body: JSON.stringify({ addr: addr, captcha: captcha }),
-						};
-						var failed = false
-						// fetch
-						const response = await fetch(
-							'https://banbucket.herokuapp.com/api/v1/claim',
-							requestOptions
-						).catch((_) => {
+							body: JSON.stringify({ action: "account_get", key: addr }),
+						}).catch((_err) => {
 							setShowMsg(true);
 							setLoading(false);
 							setIsErr(true);
-							setMsg("Failed to connect to server")
+							setMsg("Invalid Banano Address")
 							failed = true
-						});
+						})
 
 						if (!failed) {
-							const data = await response.json();
+							const requestOptions = {
+								method: 'POST',
+								headers: {
+									'Content-Type': 'application/json',
+								},
+								body: JSON.stringify({ addr: addr, captcha: captcha }),
+							};
+							var failed = false
+							// fetch
+							const response = await fetch(
+								'https://banbucket.herokuapp.com/api/v1/claim',
+								requestOptions
+							).catch((_) => {
+								setShowMsg(true);
+								setLoading(false);
+								setIsErr(true);
+								setMsg("Failed to connect to server")
+								failed = true
+							});
 	
-							setShowMsg(true);
-							setLoading(false);
-	
-							setIsErr(response.status === 400 || response.status === 500);
-	
-							setMsg(data.message);
+							if (!failed) {
+								const data = await response.json();
+		
+								setShowMsg(true);
+								setLoading(false);
+		
+								setIsErr(response.status === 400 || response.status === 500);
+		
+								setMsg(data.message);
+							}
 						}
-
 					}}
 				>
 					Claim Free Banano
