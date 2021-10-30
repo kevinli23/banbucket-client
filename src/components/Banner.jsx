7b9a783f7dae63edfcf45818e357afd16fe9a5d5
Kevin <cskevinli23@gmail.com>
2021-10-30 17:50:21 -0400

    add adsense

---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Banner = () => {
+	return (
+		<div
+			style={{
+				marginTop: '10px',
+			}}
+		>
+		<ins class="adsbygoogle"
+		style={{
+			display: "inline-block",
+			width: "300px",
+			height: '90px'
+		}}
+				data-ad-client="ca-pub-9039997426312122"
+				data-ad-slot="3789810507"></ins>
+		</div>
+	);
+};
+
+export default Banner;
