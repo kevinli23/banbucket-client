e5c811d857cd219d9c2dc1b786f294524a74de55
Kevin <cskevinli23@gmail.com>
2021-11-12 14:23:02 -0500

    remove google adsense and add meta tags

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,19 +2,7 @@ import React from 'react';
 
 const Banner = () => {
 	return (
-		<div
-			style={{
-				marginTop: '10px',
-			}}
-		>
-		<ins class="adsbygoogle"
-		style={{
-			display: "inline-block",
-			width: "300px",
-			height: '90px'
-		}}
-				data-ad-client="ca-pub-9039997426312122"
-				data-ad-slot="3789810507"></ins>
+		<div>
 		</div>
 	);
 };
---
+++ src/pages/StatsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const insightsURL =
+	'https://api.yellowspyglass.com/yellowspyglass/insights/ban_1j3rqseffoin7x5z5y1ehaqe1n7todza41kdf4oyga8phps3ea31u39ruchu';
+const dailyInfo = 'https://banbucket.herokuapp.com/api/v1/stats';
+
+const StatsPage = () => {};
+
+export default StatsPage;
