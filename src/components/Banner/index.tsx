6469358041b2316dbf6e1984adade042a443a0c5
Kevin <cskevinli23@gmail.com>
2022-01-08 17:12:28 -0500

    add sponsor request banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Link, Text, Alert, AlertIcon } from '@chakra-ui/react';
 
 const Banner = () => {
-	return <Box />;
+	return (
+		<Alert
+			status="info"
+			maxWidth="350px"
+			mt="30px"
+			mb="-55px"
+			borderRadius="15px"
+		>
+			<AlertIcon />
+			<Text>
+				Banbucket is seeking a sponsor. If you would like to advertise, please reach
+				out on{' '}
+				<Link
+					href="https://www.reddit.com/user/campuspizza"
+					target="_blank"
+					color="#CC5500"
+				>
+					reddit
+				</Link>{' '}
+				or by{' '}
+				<Link href="mailto:aiglette_carbone@aleeas.com" color="#CC5500">
+					{' '}
+					email
+				</Link>
+			</Text>
+		</Alert>
+	);
 };
 
 export default Banner;
